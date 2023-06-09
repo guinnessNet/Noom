bf1c746ef16a5124dfc09690a39eb2ca6c2b7cae
jay Kang <guinnessop@gmail.com>
2023-06-09 13:08:40 +0900

    second

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
-import express from 'express';
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'))
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => res.render('home'))
-app.get('/*', (req, res) => res.redirect('/'))
-const handleListen= () => console.log(`listening on http://localhost:3000`)
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("connected to browser");
+  socket.on("close", () => console.log("disconnected from browser"))
+  socket.on("message", (message) => {console.log(message.toString())})
+  socket.send("hello!!!");
+});
+
+server.listen(3000, handleListen);
